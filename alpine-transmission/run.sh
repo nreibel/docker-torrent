9050ee2d7bbd478869de1a0dd64bd43b3fd24e86
Nicolas Reibel <nicolas.reibel@gmail.com>
2020-01-24 01:35:41 +0100

    Create Transmission image based on Alpine

---
+++ alpine-transmission/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ../config.sh
+
+docker run \
+    -dit \
+    --restart unless-stopped \
+    --publish 9091:9091 \
+    --privileged \
+    --device=/dev/net/tun \
+    --dns=8.8.8.8 \
+    --dns=8.8.4.4 \
+    --name $TORRENT_NAME \
+    --network $NETWORK \
+    --ip 10.1.0.2 \
+    -v $TORRENT_FOLDER:/public \
+    -v $VOLUME:/var/lib/transmission-daemon \
+    seedbox/alpine-transmission
